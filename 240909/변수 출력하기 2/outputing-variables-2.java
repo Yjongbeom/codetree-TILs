f6d3d9b9a088de5238fca530a054fbac23c5a36e
Yjongbeom <139727424+Yjongbeom@users.noreply.github.com>
2024-09-09 19:17:49 +0900

    Title: 변수 출력하기 2, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240909/변수 출력하기 2/outputing-variables-2.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        String c = "C";
+        int a = 3;
+
+        System.out.println(c + "!.....!" + a);
+    }
+}
