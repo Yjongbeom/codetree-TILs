0d08747ca3676a4d40e7071ead8e66ae739870c6
Yjongbeom <139727424+Yjongbeom@users.noreply.github.com>
2024-09-11 03:31:27 +0900

    Title: 낮은 지점들, Time: 1542ms, Memory: 57MB, Status: Passed - CodeTree

--- 240911/낮은 지점들/lowest-points.java
+++ 240911/낮은 지점들/lowest-points.java
@@ -6,7 +6,7 @@ public class Main {
         HashMap<Integer, Integer> h = new HashMap<>();
 
         int n = sc.nextInt();
-        int sum = 0;
+        long sum = 0;
 
         for(int i=0; i<n; i++)
         {
