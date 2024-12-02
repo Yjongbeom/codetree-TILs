7071839d10280597af7f136160d5d92003961fd8
Yjongbeom <139727424+Yjongbeom@users.noreply.github.com>
2024-12-02 22:52:35 +0900

    Title: 차이가 가장 작은 수, Time: 1380ms, Memory: 34MB, Status: Passed - CodeTree

--- 241202/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
+++ 241202/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
@@ -1,28 +1,55 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.TreeSet;
 
 public class Main {
+    public static final int INT_MAX = Integer.MAX_VALUE;
+    public static final int MAX_N = 100000;
+    
+    // 변수 선언
+    public static int n, m;
+    public static int[] arr = new int[MAX_N];
+    
+    public static TreeSet<Integer> s = new TreeSet<>();
+    
+    // 답을 저장합니다.
+    public static int ans = INT_MAX;
+
     public static void main(String[] args) {
-        TreeSet<Integer> h = new TreeSet<>();
         Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        m = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        // 입력으로 주어진 숫자를 전부 treeset에 넣어줍니다.
+        for(int i = 0; i < n; i++)
+            s.add(arr[i]);
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        // 각 숫자 x 대해
+        // x보다 m 이상 더 크면서 가장 작은 값과
+        // x보다 m 이상 더 작으면서 가장 큰 값을 구해
+        // 차이가 가장 작은 경우를 갱신합니다.
+        for(int i = 0; i < n; i++) {
+            int x = arr[i];
 
-        for (int i = 0; i < n; i++) {
-            h.add(sc.nextInt());
+            // x보다 m 이상 더 크면서 가장 작은 값은
+            // r - x >= m를 만족하는 최소 r이므로
+            // r >= m + x을 만족하는 최소 r을 구하면 됩니다.
+            if(s.ceiling(m + x) != null)
+                ans = Math.min(ans, s.ceiling(m + x) - x);
+
+            // x보다 m 이상 더 작으면서 가장 큰 값은
+            // x - r >= m를 만족하는 최대 r이므로
+            // r <= x - m을 만족하는 최대 r을 구하면 됩니다.
+            if(s.floor(x - m) != null)
+                ans = Math.min(ans, x - s.floor(x - m));
         }
 
-        int min = Integer.MAX_VALUE;
-        boolean found = false;
+        // 불가능하다면 -1을 넣어줍니다.
+        if(ans == INT_MAX)
+            ans = -1;
 
-        for (int num : h) {
-            Integer higher = h.ceiling(num + m);
-            if (higher != null) {
-                found = true;
-                min = Math.min(min, higher - num);
-            }
-        }
-
-        System.out.println(found ? min : -1);
+        System.out.print(ans);
     }
 }
