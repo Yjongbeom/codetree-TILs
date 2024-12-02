cf0602668885d9f9f05df5cf269ac4cce994a0ee
Yjongbeom <139727424+Yjongbeom@users.noreply.github.com>
2024-12-02 21:18:21 +0900

    Title: 자리 차지하기, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241202/자리 차지하기/take-place.java
+++ 241202/자리 차지하기/take-place.java
@@ -1,19 +1,35 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        TreeSet<Integer> h = new TreeSet<>();
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int n = sc.nextInt();
+        int m = sc.nextInt();
 
-        for(int i=0; i<a; i++)
-        {
-            int p = sc.nextInt();
-            
-            h.add(p);
+        TreeSet<Integer> chairs = new TreeSet<>();
+        for (int i = 1; i <= m; i++) {
+            chairs.add(i);
         }
-        System.out.println(h.size());
-        
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            int ai = sc.nextInt();
+
+            Integer chair = chairs.ceiling(1);
+
+            if (chair != null && chair <= ai) 
+            {
+                chairs.remove(chair);
+                count++;
+            } 
+            else 
+            {
+                break;
+            }
+        }
+
+        System.out.println(count);
     }
 }
