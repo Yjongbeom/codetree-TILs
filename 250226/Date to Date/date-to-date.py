af279f426d94c54f52f418e727c7e6a845750bbc
Yjongbeom <139727424+Yjongbeom@users.noreply.github.com>
2025-02-26 19:39:56 +0900

    Title: Date to Date, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250226/Date to Date/date-to-date.py
+++ 250226/Date to Date/date-to-date.py
@@ -2,19 +2,16 @@ m1, d1, m2, d2 = map(int, input().split())
 
 dates = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-cnt = 0
+cnt = 1
 while True:
+    if m1 == m2 and d1 == d2:
+        break
+
     d1 += 1
     cnt += 1
 
     if m1 != m2 and d1 > dates[m1]:
         m1 += 1
         d1 = 1
-
-    if m1 == m2 and d1 == d2:
-        cnt += 1
-        break
-
-    
 
 print(cnt)
